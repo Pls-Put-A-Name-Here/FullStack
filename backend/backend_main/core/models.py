f0cfbd2673b5983782459908ab4fac840d9ed2a8
Joe342wise <jnyarko342@gmail.com>
2024-03-11 12:58:41 +0000

    I create 5 modules for 5 tables in the database

--- backend/backend_main/core/models.py
+++ backend/backend_main/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 # Bright Start
 # This is the model for tblUsers
@@ -37,3 +38,65 @@ class ProductCategoryTable(models.Model):
     created_date = models.DateTimeField(auto_now=True)
     last_edited_date = models.DateTimeField(auto_now=True)
 # Bright ends
+
+# JoeWise Starts
+# This is the model for Order status table
+class OrderStatusTable(models.Model):
+    order_status_name = models.CharField(max_length=50)
+    order_status_description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_edited_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.order_status_name
+
+# This is the model for Status table
+class StatusTable(models.Model):
+    status_name = models.CharField(max_length=50)
+    status_description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_edited_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.status_name
+
+# This is the model for Order table
+class OrderTable(models.Model):
+    order_customer_id = models.ForeignKey(CustomerTable, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    order_delivery_address = models.CharField(max_length=255)
+    order_total_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    order_status = models.ForeignKey(OrderStatusTable, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_edited_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.order_customer_id)
+
+# This is the model for Order Items table
+class OrderItemsTable(models.Model):
+    order_id = models.ForeignKey(OrderTable, on_delete=models.CASCADE)
+    product_id = models.ForeignKey('ProductTable', on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_edited_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.order_id)
+
+# This is the model for supplier table
+class SupplierTable(models.Model):
+    supplier_name = models.CharField(max_length=100, null=False)
+    supplier_contact_info = models.CharField(max_length=255)
+    supplier_address_line1 = models.CharField(max_length=255)
+    supplier_address_line2 = models.CharField(max_length=255)
+    supplier_city = models.CharField(max_length=100)
+    supplier_state = models.CharField(max_length=100)
+    supplier_postal_code = models.CharField(max_length=20)
+    supplier_country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.supplier_name
+# JoeWise Ends
