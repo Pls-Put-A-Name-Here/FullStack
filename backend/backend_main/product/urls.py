77bc5c7db0d3c92411b4b40c63497aee86a04fb2
Adjanour <adjanour@icloud.com>
2024-03-30 17:19:45 +0000

    adds initial setup

---
+++ backend/backend_main/product/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from product.views import ProductView
+
+router = DefaultRouter()
+router.register(r'products',ProductView)
+
+
+urlpatterns = [
+    path('',include(router.urls))
+]
--- backend/backend_main/product/views.py
+++ backend/backend_main/product/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from product.models import Product
+from product.serializers import ProductSerializer
 
 # Create your views here.
+class ProductView(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
