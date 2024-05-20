06bb093715ea1d061b106f0ecb9e1b98c438c106
Adjanour <adjanour@icloud.com>
2024-05-20 09:57:42 +0000

    adding database objects

---
+++ backend/backend_scripts/Tables/dbo.tblSuppliers.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblSuppliers] (
+  [supIdpk] [int] IDENTITY,
+  [supName] [nvarchar](100) NOT NULL,
+  [supContactInfo] [nvarchar](255) NULL,
+  [supAddressLine1] [nvarchar](255) NULL,
+  [supAddressLine2] [nvarchar](255) NULL,
+  [supCity] [nvarchar](100) NULL,
+  [supState] [nvarchar](100) NULL,
+  [supPostalCode] [nvarchar](20) NULL,
+  [supCountry] [nvarchar](100) NULL,
+  PRIMARY KEY CLUSTERED ([supIdpk])
+)
+ON [PRIMARY]
+GO
