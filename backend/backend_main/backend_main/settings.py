b0774857c4acdd0c88b5540eabd99c515d835351
joe342wise <jnyarko342@gmail.com>
2024-05-21 15:18:55 +0000

    chore: Add rest_framework to installed apps and update requirements

--- backend/backend_main/backend_main/settings.py
+++ backend/backend_main/backend_main/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'product',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -82,7 +83,7 @@ DATABASES = {
         "USER":"",
         "PASSWORD":"",
        # change host name to the name you see when you are trying to login to ssms
-        "HOST":"KIRKPC\SALEMSERVER",
+        "HOST":"JOEWISEPC\SQLEXPRESS",
         "PORT": "",
         "OPTIONS": {"driver": "ODBC Driver 17 for SQL Server"},
     }
