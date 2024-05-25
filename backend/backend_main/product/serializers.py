6a1c9129df38fe0427a5c85214dd3df7e1302287
Adjanour <adjanour@icloud.com>
2024-05-25 20:10:23 +0000

    feat: Add lazy loading for images and update gitignore
    The code changes include adding the `.idea/` directory to the `.gitignore` file and updating the `views.py` file in the `cart` app. Additionally, the `ProductConfig` class in the `product/apps.py` file was renamed from `ProductsConfig`. The `urls.py` file in the `product` app was also modified to include new paths for `ProductImageView`. Lastly, the `models.py` file in the `cart` app was updated to fix a typo and add a new model for `CartItem`.
    
    feat: add product-image uploads and serialization

--- backend/backend_main/product/serializers.py
+++ backend/backend_main/product/serializers.py
@@ -1,10 +1,43 @@
 from rest_framework import serializers
-from product.models import Product
+from .models import Product, ProductImage
+
 
 class ProductSerializer(serializers.ModelSerializer):
+    # fetch the product images
+    # since the product images are a related field, we need to use the SerializerMethodField
+    product_images = serializers.SerializerMethodField()
+
+    # fetch other related fields using prefetch_related
+    brand = serializers.StringRelatedField()
+    category = serializers.StringRelatedField()
+    subcategory = serializers.StringRelatedField()
 
     class Meta:
-        model= Product
-        fields="__all__"
+        model = Product
+        fields = "__all__"
 
-        
+    # might have to optimize this later
+    # takes about 6 seconds to load 1000 products
+    # by default, the SerializerMethodField will look for a method called get_<field_name>
+    def get_product_images(self, obj):
+        images = obj.productimage_set.all()
+        return ProductImageSerializer(images, many=True).data
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = "__all__"
+
+
+# something cool from a stackoverflow post
+# might be useful for adding extra fields to serializers
+class CustomSerializer(serializers.HyperlinkedModelSerializer):
+
+    def get_field_names(self, declared_fields, info):
+        expanded_fields = super(CustomSerializer, self).get_field_names(declared_fields, info)
+
+        if getattr(self.Meta, 'extra_fields', None):
+            return expanded_fields + self.Meta.extra_fields
+        else:
+            return expanded_fields
